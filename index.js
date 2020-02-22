5d88bcbee1ed083598281e4171457d35cf04c3a7
richard <richard.turra@sensedata.com.br>
2020-02-21 23:37:34 -0300

    login verify

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+var cors = require('cors')
 const jwt = require('jsonwebtoken')
 
 const app = express();
+app.use(cors())
+app.use(express.json())
+const secretKey = 'secretkey'
 
 app.get('/api', (req, res) => {
     res.json({
@@ -10,16 +14,34 @@ app.get('/api', (req, res) => {
 })
 
 app.post('/api/login', (req, res) => {
+
+    const user = req.body.user
+    const password = req.body.password
+
     // mock user
-    const user = {
+    const users = {
         id: 1,
         username: 'dev',
+        password: 'dev',
         email: 'stdevelopr@gmail.com'
     }
+    if (users.username == user && users.password == password) {
 
-    jwt.sign({ user }, 'secretkey', (err, token) => {
-        res.json({ token })
-    })
+        jwt.sign({ user }, secretKey, (err, token) => {
+            res.status(200).json({ token })
+        })
+    }
+    else res.status(401).send({ error: 'invalid user' })
 })
 
-app.listen(5000, () => console.log('server started on port 5000'))
+app.post('/api/login/verify', (req, res) => {
+    const token = req.body.token
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        res.send(true)
+    } catch (err) {
+        res.send(false)
+    }
+})
+
+app.listen(3000, () => console.log('server started on port 3000'))
